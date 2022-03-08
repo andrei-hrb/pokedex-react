5323de146add7124147c8147edcfb63009052f26
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-09 01:20:27 +0200

    header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+        <footer className='text-center text-xs'>
+            Created with lots of ❤️ and 🎮 by{' '}
+            <a href='https://hirbu.ro/' className='text-sky-500'>
+                Hîrbu Andrei
+            </a>
+            . Check the{' '}
+            <a
+                href='https://github.com/andrei-hrb/pokedex-react'
+                className='text-sky-500'
+            >
+                <code>source code</code>
+            </a>{' '}
+            on Github.
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => (
+    <header>
+        <h2 className='text-4xl font-black'>Pokédex</h2>
+    </header>
+)
+
+export default Header
